19998554dc76ac1d86d7dbb24dbfc169bbd0fa4b
evbishop <anddenissyuk@gmail.com>
2022-01-12 13:55:00 +0600

    Put PlayerShooting in a separate class

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,21 +5,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Header("Player")]
     [SerializeField] int health = 200;
     [SerializeField] GameObject deathVFX = null;
     [SerializeField] float durationOfExplosion = 1f;
     [SerializeField] AudioClip deathSound = null;
     [SerializeField] [Range(0, 1)] float deathSoundVolume = 0.75f;
-    [SerializeField] AudioClip shootSound = null;
-    [SerializeField] [Range(0, 1)] float shootSoundVolume = 0.25f;
-
-    [Header("Projectile")]
-    [SerializeField] GameObject laserPrefab = null;
-    [SerializeField] float projectileSpeed = 10f;
-    [SerializeField] float projectileFiringPeriod = 0.5f;
-    
-    Coroutine firingCoroutine;
 
     public int Health { get { return health; } }
 
@@ -35,11 +25,6 @@ public class Player : MonoBehaviour
         OnPlayerDeath -= Die;
     }
 
-    void Update()
-    {
-        Fire();
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
@@ -47,25 +32,6 @@ public class Player : MonoBehaviour
         health -= damageDealer.Damage;
         damageDealer.Hit();
         if (health <= 0) OnPlayerDeath?.Invoke();
-    }
-
-    void Fire()
-    {
-        if (Input.GetButtonDown("Fire1"))
-            firingCoroutine = StartCoroutine(FireContinuosly());
-        if (Input.GetButtonUp("Fire1"))
-            StopCoroutine(firingCoroutine);
-    }
-
-    IEnumerator FireContinuosly()
-    {
-        while (true)
-        {
-            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
-            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
-            AudioSource.PlayClipAtPoint(shootSound, Camera.main.transform.position, shootSoundVolume);
-            yield return new WaitForSeconds(projectileFiringPeriod);
-        }
     }
 
     void Die()
---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] AudioClip shootSound;
+    [SerializeField] [Range(0, 1)] float shootSoundVolume = 0.2f;
+    [SerializeField] float projectileSpeed = 30f, projectileFiringPeriod = 0.1f;
+    Coroutine firingCoroutine;
+
+    void Update()
+    {
+        Fire();
+    }
+
+    void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            firingCoroutine = StartCoroutine(FireContinuosly());
+        if (Input.GetButtonUp("Fire1"))
+            StopCoroutine(firingCoroutine);
+    }
+
+    IEnumerator FireContinuosly()
+    {
+        while (true)
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            AudioSource.PlayClipAtPoint(shootSound, Camera.main.transform.position, shootSoundVolume);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
+    }
+}
